651b921eae887dfbf43e960551720f66b4d22eb0
Paul Bundi Marangu <paul1bundi@gmail.com>
2026-04-13 20:06:47 +0300

    Refactor UserTableSeeder to create multiple admin users with specific details

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -15,15 +15,33 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        $user = User::factory()->create([
-            'email' => 'admin@admin.com',
-            'account_type' => User::TYPE_ADMIN,
-            'organization_id' => null,
-            'phone' => '0713240494',
-            'password'=>bcrypt('admin')
-        ]);
+        $adminRole = Role::first()->id;
 
-        $user->roles()->attach(Role::first()->id);
+        $admins = [
+            [
+                'email' => 'mrembeka@gmail.com',
+                'first_name' => 'Julie',
+                'last_name' => 'Eluid',
+                'account_type' => User::TYPE_ADMIN,
+                'organization_id' => null,
+                'phone' => '0789311440',
+                'password' => bcrypt('Mrembeka@123'),
+            ],
+            [
+                'email' => 'mwanjeriloice@gmail.com',
+                'first_name' => 'Millicent',
+                'last_name' => 'Wanjeri',
+                'account_type' => User::TYPE_ADMIN,
+                'organization_id' => null,
+                'phone' => '0719834595',
+                'password' => bcrypt('Mwanjeri@123'),
+            ],
+        ];
+
+        foreach ($admins as $adminData) {
+            $user = User::factory()->create($adminData);
+            $user->roles()->attach($adminRole);
+        }
 
         User::factory(20)->hasAddresses(4)->create();
     }

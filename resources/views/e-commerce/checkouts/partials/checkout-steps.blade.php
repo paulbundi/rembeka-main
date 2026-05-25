dc6adece4491ca93424112c511daebb64fceddca
alexbart <alexbart695@gmail.com>
2026-05-25 10:45:19 +0300

    Fix checkout stepper

--- resources/views/e-commerce/checkouts/partials/checkout-steps.blade.php
+++ resources/views/e-commerce/checkouts/partials/checkout-steps.blade.php
@@ -1,26 +1,34 @@
+@php
+    $route = \Illuminate\Support\Facades\Route::currentRouteName();
+@endphp
+
 <div class="steps steps-light pt-2 pb-3 mb-5">
-    <a class="step-item @if(\Illuminate\Support\Facades\Route::currentRouteName() == 'checkout.cart' || \Illuminate\Support\Facades\Route::currentRouteName() == 'checkout.details' || \Illuminate\Support\Facades\Route::currentRouteName() == 'payment.mode') active @endif" href="{{ route('checkout.cart') }}">
+
+    {{-- STEP 1 --}}
+    <a class="step-item {{ in_array($route, ['checkout.cart', 'checkout.details', 'payment.mode']) ? 'active' : '' }}"
+        href="{{ route('checkout.cart') }}">
         <div class="step-progress"><span class="step-count">1</span></div>
         <div class="step-label"><i class="ci-cart"></i>Cart</div>
     </a>
-    
-    <a class="step-item current @if(\Illuminate\Support\Facades\Route::currentRouteName() == 'checkout.details' || \Illuminate\Support\Facades\Route::currentRouteName() == 'payment.mode') active @endif }}">
+
+    {{-- STEP 2 --}}
+    <a class="step-item {{ in_array($route, ['checkout.details', 'payment.mode']) ? 'active' : '' }}">
         <div class="step-progress"><span class="step-count">2</span></div>
         <div class="step-label"><i class="ci-user-circle"></i>Details</div>
     </a>
-    
-    <!-- <a class="step-item" href="checkout-shipping.html">
-        <div class="step-progress"><span class="step-count">3</span></div>
-        <div class="step-label"><i class="ci-package"></i>Shipping</div>
-    </a> -->
-    
-    <a class="step-item @if(\Illuminate\Support\Facades\Route::currentRouteName() == 'payment.mode') active @endif" href="#">
+
+    {{-- STEP 3 (optional - shipping skipped) --}}
+
+    {{-- STEP 4 --}}
+    <a class="step-item {{ $route == 'payment.mode' ? 'active' : '' }}">
         <div class="step-progress"><span class="step-count">4</span></div>
         <div class="step-label"><i class="ci-card"></i>Payment</div>
     </a>
-    
-    <a class="step-item" href="#">
+
+    {{-- STEP 5 --}}
+    <a class="step-item">
         <div class="step-progress"><span class="step-count">5</span></div>
         <div class="step-label"><i class="ci-check-circle"></i>Review</div>
     </a>
+
 </div>
